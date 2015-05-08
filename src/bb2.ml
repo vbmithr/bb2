e4226e6249406e869dd5f02a7dfb72074ea64c99
Vincent Bernardoff <vb@luminar.eu.org>
2015-05-08 20:47:51 +0200

    Added Cryptys's analysis.

--- src/bb2.ml
+++ src/bb2.ml
@@ -16,7 +16,6 @@ module D = Graphviz.Dot
       let graph_attributes _ = [`Concentrate true]
     end)
 
-module B = Bittrex.Bittrex(Bittrex_async.Bittrex)
 module BF = Path.BellmanFord(G)
     (struct
       type edge = G.E.t
@@ -29,7 +28,8 @@ module BF = Path.BellmanFord(G)
 
 module StringSet = Set.Make(String)
 
-let main () =
+let bittrex () =
+  let module B = Bittrex.Bittrex(Bittrex_async.Bittrex) in
   let g = G.create () in
   B.MarketSummary.summaries () >>= fun markets ->
   Format.printf "Found %d markets.@." @@ List.length markets;
@@ -43,10 +43,10 @@ let main () =
       currs := StringSet.add !currs c2;
       let c1 = if Char.is_digit c1.[0] then "\"" ^ c1 ^ "\"" else c1 in
       let c2 = if Char.is_digit c2.[0] then "\"" ^ c2 ^ "\"" else c2 in
-      (* Format.printf "Processing %s-%s %f %f.@." c1 c2 m.bid m.ask; *)
+      Format.printf "Processing %s-%s %f %f.@." c1 c2 m.bid m.ask;
       G.add_vertex g c1;
       G.add_vertex g c2;
-      G.add_edge_e g (G.E.create c1 m.ask c2);
+      G.add_edge_e g (G.E.create c1 (1. /. m.ask) c2);
       G.add_edge_e g (G.E.create c2 m.bid c1)
     );
 
@@ -60,10 +60,53 @@ let main () =
     );
   D.fprint_graph Format.str_formatter g;
   let contents = Format.flush_str_formatter () in
-  Writer.save "graph.dot" ~contents >>| fun () ->
+  Writer.save "bittrex.dot" ~contents >>| fun () ->
   Format.printf "Done processing.@."
 
+let cryptsy () =
+  let module C = Bittrex.Cryptsy(Bittrex_async.Cryptsy) in
+  let g = G.create () in
+  let ht = Int.Table.create () in
+  let currs = ref StringSet.empty in
+  C.Ticker.tickers () >>= fun tickers ->
+  List.iter tickers ~f:(fun t ->
+      let open C.Ticker in
+      Hashtbl.add_exn ht ~key:t.id ~data:t
+    );
+  C.Market.markets () >>= fun markets ->
+  List.iter markets ~f:(fun m ->
+      let open C.Market in
+      let slash = String.index_exn m.label '/' in
+      let c1 = String.sub m.label 0 slash in
+      let c2 = String.sub m.label (slash+1) (String.length m.label - slash - 1) in
+      currs := StringSet.add !currs c1;
+      currs := StringSet.add !currs c2;
+      let c1 = if Char.is_digit c1.[0] then "\"" ^ c1 ^ "\"" else c1 in
+      let c2 = if Char.is_digit c2.[0] then "\"" ^ c2 ^ "\"" else c2 in
+      let bidask = Hashtbl.find_exn ht m.id in
+      let open C.Ticker in
+      Format.printf "Processing %s-%s %f %f.@." c1 c2 bidask.bid bidask.ask;
+      G.add_vertex g c1;
+      G.add_vertex g c2;
+      G.add_edge_e g (G.E.create c1 (1. /. bidask.ask) c2);
+      G.add_edge_e g (G.E.create c2 bidask.bid c1)
+    );
+
+  StringSet.iter !currs ~f:(fun c ->
+      (* Format.printf "Searching for negative cycle starting from %s.@." c; *)
+      let cycle = try
+          BF.find_negative_cycle_from g c
+        with Not_found -> [] in
+      if cycle <> [] then
+        Format.printf "Negative cycle found %s.@." @@
+        String.concat ~sep:" -> "
+          (List.map cycle ~f:(fun (start,v,stop) -> start ^ " " ^ Float.to_string v))
+    );
+  D.fprint_graph Format.str_formatter g;
+  let contents = Format.flush_str_formatter () in
+  Writer.save "cryptsy.dot" ~contents >>| fun () ->
+  Format.printf "Done processing.@."
 
 let _ =
-  don't_wait_for @@ main ();
+  don't_wait_for @@ cryptsy ();
   never_returns @@ Scheduler.go ()
